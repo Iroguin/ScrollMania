80250e8c31f9780c7722aaf6485c46cc7125f05e
Iroguin <eddie@rimey.com>
2023-08-31 20:22:53 +0300

    modified car physics

--- game_objects/cars/scripts/carcontroller.gd
+++ game_objects/cars/scripts/carcontroller.gd
@@ -1,7 +1,10 @@
 extends VehicleBody3D
 
-@export var max_brake:float = 50
+@export var max_brake:float = 20
 @export var max_speed:float = 300
+
+#TODO: do this wheel grip thing with some inverse logarythmic math so speed equals less grip
+@export var wheel_grip:float = 0
 var turning_radius:float = 0.0
 
 # Called when the node enters the scene tree for the first time.
@@ -19,15 +22,23 @@ func _physics_process(delta):
 		$right_rear.brake = max_brake
 		$left_rear.engine_force = 0
 		$right_rear.engine_force = 0
+		$right_rear.wheel_friction_slip = 0
+		$left_rear.wheel_friction_slip = 0
+		$right_front.wheel_friction_slip = 1.0
+		$left_front.wheel_friction_slip = 1.0
 	else:
 		$left_rear.brake = 0
 		$right_rear.brake = 0
 		$left_rear.engine_force = max_speed
 		$right_rear.engine_force = max_speed
+		$right_rear.wheel_friction_slip = 6.5
+		$left_rear.wheel_friction_slip = 6.5
+		$right_front.wheel_friction_slip = 8.5
+		$left_front.wheel_friction_slip = 8.5
 	if Input.is_action_just_pressed("scroll_left"):
-		turning_radius += 4.0
+		turning_radius += 8.75
 	if Input.is_action_just_pressed("scroll_right"):
-		turning_radius -= 4.0
-	turning_radius = clamp(turning_radius,-25, 25)
+		turning_radius -= 8.75
+	turning_radius = clamp(turning_radius,-35, 35)
 	$left_front.steering = deg_to_rad(turning_radius)
 	$right_front.steering = deg_to_rad(turning_radius)

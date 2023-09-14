74b661aba1a8a56403046a98e2c92878e5cabb48
Iroguin <eddie@rimey.com>
2023-09-14 20:20:50 +0300

    car physics broken now

--- game_objects/cars/scripts/carcontroller.gd
+++ game_objects/cars/scripts/carcontroller.gd
@@ -1,6 +1,6 @@
 extends VehicleBody3D
 
-@export var max_brake:float = 20
+@export var max_brake:float = 20000
 @export var max_speed:float = 600
 
 #TODO: do this wheel grip thing with some inverse logarythmic math so speed equals less grip
@@ -26,26 +26,33 @@ func _physics_process(delta):
 	if Input.is_action_pressed("scroll_press"):
 		time += delta
 		if time > 0.5:
+			is_drifting = false
 			$left_rear.brake = max_brake
 			$right_rear.brake = max_brake
+			$left_rear.engine_force = 0.0
+			$right_rear.engine_force = 0.0
+			print(is_drifting)
+	else:
+		time = 0.0
+		$left_rear.brake = 0
+		$right_rear.brake = 0
+		$left_rear.engine_force = max_speed
+		$right_rear.engine_force = max_speed
+		
 	if Input.is_action_just_pressed("scroll_press"):
 		is_drifting = !is_drifting
 	
 	if is_drifting == true:
 		print(transform.basis.z)
-		apply_central_force(transform.basis.z * 300)
+		apply_central_force(transform.basis.z * 400)
 		$left_rear.engine_force = 0.0
 		$right_rear.engine_force = 0.0
-		$right_rear.wheel_friction_slip = 0.0
-		$left_rear.wheel_friction_slip = 0.0
+		$right_rear.wheel_friction_slip = 0.1
+		$left_rear.wheel_friction_slip = 0.1
 		$right_front.wheel_friction_slip = 2.0
 		$left_front.wheel_friction_slip = 2.0
 		
 	if is_drifting == false:
-		$left_rear.brake = 0
-		$right_rear.brake = 0
-		$left_rear.engine_force = max_speed
-		$right_rear.engine_force = max_speed
 		$right_rear.wheel_friction_slip = 6.5
 		$left_rear.wheel_friction_slip = 6.5
 		$right_front.wheel_friction_slip = 8.5

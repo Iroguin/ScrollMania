e6895cb5e6b91f58e1bf137a389d0a2802aa6f19
Iroguin <eddie@rimey.com>
2023-10-05 18:46:42 +0300

    last update

--- game_objects/cars/scripts/carcontroller.gd
+++ game_objects/cars/scripts/carcontroller.gd
@@ -1,8 +1,9 @@
 extends VehicleBody3D
 
-@export var max_brake:float = 50 + linear_velocity.z
-@export var max_speed:float = 100
-@export var global_grip:float = 1 + linear_velocity.z
+@export var max_brake:float = 50 
+@export var max_speed:float = 30
+@export var accel:float = 100
+@export var global_grip:float = 1 
 
 #TODO: do this wheel grip thing with some inverse logarythmic math so speed equals less grip
 @export var wheel_grip:float = 0
@@ -26,41 +27,47 @@ func _process(_delta):
 
 
 func _physics_process(delta):
-	print(linear_velocity.z)
+	global_grip = linear_velocity.length()
+	if linear_velocity.length() > max_speed:
+		$left_rear.engine_force = 0.0
+		$right_rear.engine_force = 0.0
+	else: 
+		$left_rear.engine_force = accel
+		$right_rear.engine_force = accel
 	if Input.is_action_pressed("scroll_press"):
 		time += delta
 		if time > 0.2:
 			is_drifting = false
 			$left_rear.brake = max_brake
 			$right_rear.brake = max_brake
+			$left_front.brake = max_brake
+			$right_front.brake = max_brake
 			$left_rear.engine_force = 0.0
 			$right_rear.engine_force = 0.0
-			print(is_drifting)
 	else:
 		time = 0.0
+		$left_front.brake = 0
+		$right_front.brake = 0
 		$left_rear.brake = 0
 		$right_rear.brake = 0
-		$left_rear.engine_force = max_speed
-		$right_rear.engine_force = max_speed
-		
+
 	if Input.is_action_just_pressed("scroll_press"):
 		is_drifting = !is_drifting
 	
 	if is_drifting == true:
-		print(transform.basis.z)
 		apply_central_force(-transform.basis.z * 200)
 		$left_rear.engine_force = 0.0
 		$right_rear.engine_force = 0.0
-		$right_rear.wheel_friction_slip = global_grip * 0.5
-		$left_rear.wheel_friction_slip = global_grip * 0.5
-		$right_front.wheel_friction_slip = global_grip * 0.5
-		$left_front.wheel_friction_slip = global_grip * 0.5
+		$right_rear.wheel_friction_slip = 0.5
+		$left_rear.wheel_friction_slip = 0.5
+		$right_front.wheel_friction_slip = 0.5
+		$left_front.wheel_friction_slip = 0.5
 		
 	if is_drifting == false:
-		$right_rear.wheel_friction_slip = global_grip * 1.5
-		$left_rear.wheel_friction_slip = global_grip * 1.5
-		$right_front.wheel_friction_slip = global_grip * 1.5
-		$left_front.wheel_friction_slip = global_grip * 1.5
+		$right_rear.wheel_friction_slip = 1 + global_grip * 0.2
+		$left_rear.wheel_friction_slip = 1 + global_grip * 0.2
+		$right_front.wheel_friction_slip = 1 + global_grip * 0.2
+		$left_front.wheel_friction_slip = 1 + global_grip * 0.2
 	if Input.is_action_just_pressed("scroll_left"):
 		turning_radius = lerpf(turning_radius, turning_radius + 11.666, 5 * delta)
 	if Input.is_action_just_pressed("scroll_right"):
